5a1bf837f76579b98cecc7ffa74581a23fd50116
likunn <likunn@enn.cn>
2022-09-14 18:00:26 +0800

    链表练习

--- algorithm/src/main/java/com/zbcn/algorithm/T20_LinkedTest.java
+++ algorithm/src/main/java/com/zbcn/algorithm/T20_LinkedTest.java
@@ -92,8 +92,92 @@ public class T20_LinkedTest {
      * @param node2
      * @return
      */
-    public LinkNode mergeTwoLinkNodes(LinkNode node1, LinkNode node2){
-        return null;
+    public LinkNode mergeTwoLinkNodes(LinkNode<Integer> node1, LinkNode<Integer> node2){
+        if (node1 == null && node2 == null){
+            return  null;
+        }
+
+        // 定义一个新的链表，用来标识链表的头的位置
+        LinkNode<Integer> nodeH = new LinkNode<>();
+        // 用来移动操作链表，初始位置，指向链表的头
+        LinkNode<Integer> nodeM = node1;
+        while (node1 != null && node2 != null){
+            if (node1.getData() < node2.getData()){
+                // 如果 node1 < node2 , 则 nodeM.next = node1
+                nodeM.next = node1;
+                node1 = node1.next;
+            }else {
+                nodeM.next = node2;
+                // 移动 node2. 到 node2.next
+                node2 = node2.next;
+                // 移动 nodeM 到 nodeM.next
+                nodeM = nodeM.next;
+            }
+        }
+        if (node1 == null){
+            nodeM.next = node2;
+        }else {
+            nodeM.next = node1;
+        }
+
+        return nodeH.next;
+    }
+
+    ///删除链表倒数第 n 个结点
+
+    /**
+     * 删除链表的末尾 第 tailPlace 个节点  (双指针)
+     * @param head
+     * @param tailPlace
+     * @return
+     */
+    public LinkNode removeNthFromEnd(LinkNode head, Integer tailPlace){
+        LinkNode fast = head;
+        LinkNode slow = head;
+
+        // 设置虚拟节点，保存 head 节点
+        LinkNode<Integer> virtualNode = new LinkNode();
+        virtualNode.data = -1;
+        virtualNode.next = head;
+
+        // 移动fast 节点，将其移动 tailPlace 位置
+        while (tailPlace -1 > 0){
+            fast = fast.next;
+            tailPlace --;
+        }
+
+        // fast 为尾节点时，slow 指向了要删除的目标位置， 因为 fast  已经向后移动了 tailPlace 个位置
+
+        LinkNode pre = virtualNode;
+
+        while (fast.next != null){
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        pre.next = slow.next;
+
+        return  virtualNode.getNext();
+    }
+
+
+    // 求链表的中间结点
+
+    /**
+     * 求链表的中间节点  （快慢指针法）
+     * @param head
+     * @return
+     */
+    public LinkNode findMidNode(LinkNode head){
+
+        LinkNode fast = head;
+        LinkNode slow = head;
+        // fast 一定要在前，避免空指针
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
     }
 
 
